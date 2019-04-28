55227c4ba787c17117e97c9ee7b306354ef570de
bongi23 <andrea.bongi@live.it>
2019-04-28 12:22:31 +0200

    commenti e NetworkLayer::recv

--- source/NetworkLayer.cpp
+++ source/NetworkLayer.cpp
@@ -13,25 +13,51 @@ void NetworkLayer::init(){
     system_timer_add_component(this);
 }
 
+/**
+  * Retreives packet payload data into NetworLayer::inBuffer.
+  *
+  * If a data packet is already available, then it will be returned immediately to the caller 
+  * (application layer) in the form of a PacketBuffer.
+  *
+  * @return the data received, or an empty PacketBuffer if no data is available.
+  */
+PacketBuffer NetworkLayer::recv() {
+    if(inBuffer.empty())
+        return PacketBuffer::EmptyPacket;
+    else {
+        PacketBuffer p = inBuffer.front();
+        inBuffer.pop();
+        return p;
+    }
+    
+
+}
+
 void NetworkLayer::recv_from_mac(MicroBitEvent e) {
     PacketBuffer p = mac_layer->recv();
 
     DDPacketHeader ph = *((DDPacketHeader*)(p.getBytes()));
     if(ph.type != DD_DATA) {
+        //it's a packet of the routing protocol
         switch(ph.type) {
             case DD_COMMAND:
+                // devo modificare i "parametri di sensing", evento per il livello applicazione?
                 break;
             case DD_JOIN:
                 break;
             case DD_RT_ACK: 
                 break;
             case DD_RT_INIT:
+                // se sono il sink o se ho gia processato questo init devo ignorarlo
+                // confronto il broadcast count mio e del pacchetto
+                // se questo pacchetto è nuovo memorizzo chi me l'ha mandato come rely!!
                 break;
             default: 
                 ;
         }
     }
     else {
+        // application level packet
         uint8_t *payload = p.getBytes()+sizeof(ph);
         this->inBuffer.push(PacketBuffer(payload, ph.length));
         MicroBitEvent evt(NETWORK_LAYER, NETWORK_LAYER_PACKET_RECEIVED); 
